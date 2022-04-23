b1a4b330308328545b9c922ca1587f396c20737a
Anubhav Gupta <mail.anubhav06@gmail.com>
2022-04-23 20:42:20 +0530

    improved FAQ section
    Signed-off-by: Anubhav Gupta <mail.anubhav06@gmail.com>

--- js/faq.js
+++ js/faq.js
@@ -3,6 +3,9 @@ function faqSwitch(e){
   var content = e.lastChild.previousElementSibling
   let header = e.firstElementChild
 
+
+  let x = parseInt(document.getElementById('footer-section').style.marginTop)
+
   if(content.classList == "answer")
   {
     content.classList.add("show")
@@ -11,15 +14,34 @@ function faqSwitch(e){
     e.firstElementChild.lastChild.previousElementSibling.style.transform = 'rotate(180deg)'
 
     header.firstElementChild.style.fontWeight = "700"
+    
+    let extraHeight = 0;
+    if (Math.max(window.screen.width, window.innerWidth) > 1000)
+      extraHeight = 0;
+    else 
+      extraHeight = 40;
+
+
+    document.getElementById('footer-section').style.marginTop = x + content.offsetHeight + extraHeight + 'px';
+    
   }
   else 
   {
+    let extraHeight = 0;
+    if (Math.max(window.screen.width, window.innerWidth) > 1000)
+      extraHeight = 0;
+    else 
+      extraHeight = 40;
+
+    document.getElementById('footer-section').style.marginTop = x - content.offsetHeight - extraHeight + 'px';
+
     e.firstElementChild.lastChild.previousElementSibling.style.transform = 'rotate(0deg)'
 
     content.classList.remove ("show")
     content.classList.add("answer")
     
     header.firstElementChild.style.fontWeight = "400"
+    
   
   }
 }
