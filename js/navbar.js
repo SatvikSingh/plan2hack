f63d735bd6e3d6a0dc3d81b0d2360e40f895ff53
Anubhav Gupta <mail.anubhav06@gmail.com>
2022-04-25 23:36:33 +0530

    update header team style
    Signed-off-by: Anubhav Gupta <mail.anubhav06@gmail.com>

--- js/navbar.js
+++ js/navbar.js
@@ -5,9 +5,11 @@ document.addEventListener('DOMContentLoaded', () => {
     var scrollTop = (window.pageYOffset !== undefined) ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop;
     if (scrollTop > 70){
       document.getElementsByClassName('navbar')[0].style.backgroundColor = 'black';
+      document.getElementById('nav-team').style.color = '#f4cc70';
     }
     else{
       document.getElementsByClassName('navbar')[0].style.backgroundColor = 'rgba(0, 0, 0, 0)';
+      document.getElementById('nav-team').style.color = 'white';
     }
   })
 
